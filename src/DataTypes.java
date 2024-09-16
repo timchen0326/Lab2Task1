4833a3942b683f341048fe93793a417d77314067
Tim Chen <timchen0326ca@gmail.com>
2024-09-16 12:11:25 -0400

    task 1 complete

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class DataTypes {
     public static long sum(List<Integer> numbers) {
 
-        int s = 0;
+        long s = 0;
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
